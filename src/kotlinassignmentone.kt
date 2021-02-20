0ca02d96c8e152f68839a1d9d93d8a4a0a8007f3
student <audreymunyiva@gmail.com>
2021-02-20 21:12:35 +0300

    first commit

---
+++ src/kotlinassignmentone.kt
@@ -0,0 +1,16 @@
+fun main() {var name:String= "audreyMunyiva"
+    println(name)
+
+    var age:Byte=19
+    println(age)
+
+    var phoneNumber:String="0795860190"
+    println(phoneNumber)
+
+    var weight:Float=55.5.toFloat()
+    println(weight)
+
+    var citizen:String="kenyan"
+    println(citizen)
+
+}
